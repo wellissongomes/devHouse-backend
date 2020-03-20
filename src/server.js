bd4ce057474ec34c1633ee9f5ae9574288c91e68
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-20 19:13:23 -0300

    configurando mongoose

--- src/server.js
+++ src/server.js
@@ -1,7 +1,13 @@
 import express, { json } from 'express';
+import mongoose from 'mongoose';
 import routes from './routes';
 
 const server = express();
+
+mongoose.connect('mongodb+srv://devhouse:devhouse@devhouse-xszia.mongodb.net/devhouse?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 server.use(json());
 server.use(routes);
