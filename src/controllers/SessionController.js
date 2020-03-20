17adfb763412fed783a0f721cd039dca3f9f777f
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-20 19:44:22 -0300

    add controller/model ref ao login

---
+++ src/controllers/SessionController.js
@@ -0,0 +1,15 @@
+import User from '../models/User';
+
+class SessionController {
+    async store(req, res) {
+        const { email, name } = req.body;
+
+        let user = await User.findOne({ email, name });
+        if(!user) {
+            user = await User.create({ email, name });
+        }
+        return res.json(user);
+    }
+};
+
+export default new SessionController();
