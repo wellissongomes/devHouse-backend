863d42e2b088d415463751f696e158cca3c6fc85
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-20 19:58:45 -0300

    add HouseController e House

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 
 import SessionController from './controllers/SessionController';
+import HouseController from './controllers/HouseController'
 
 const routes = new Router();
 
 routes.post('/sessions', SessionController.store);
+
+routes.post('/houses', HouseController.store);
+
 
 export default routes;
