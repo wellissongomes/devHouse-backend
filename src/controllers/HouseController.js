94dd279353af5d2b414ea987b44d13c1f5dfc960
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-21 16:40:07 -0300

    add metodo index

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,13 @@
 import House from '../models/House';
 
 const HouseController = {
+    async index(req, res) {
+        const { available } = req.query;
+        const houses = await House.find({ available })
+
+        return res.json(houses);
+    },
+
     async store(req, res) {
         const { filename } = req.file;
         const { description, price, location, available } = req.body;
