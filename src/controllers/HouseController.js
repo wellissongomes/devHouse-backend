0e29d27622f7d710c93d322ec520dad3c3a7e61d
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-21 17:00:02 -0300

    add metodo update

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,4 +1,5 @@
 import House from '../models/House';
+import User from '../models/User';
 
 const HouseController = {
     async index(req, res) {
@@ -23,6 +24,31 @@ const HouseController = {
         });
 
         return res.json(house);
+    },
+
+    async update(req, res){
+        const { filename } = req.file;
+        const { house_id } = req.params;
+        const { description, price, location, available } = req.body;
+        const { user_id } = req.headers;
+
+        const user = await User.findById(user_id);
+        const house = await House.findById(house_id);
+
+        if(String(user._id) !== String(house.user)) {
+            return res.status(401).json({ error: 'Não autorizado.' });
+        }
+
+        await House.updateOne({ _id: house_id }, {
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            available
+        });
+        
+        return res.send();
     }
 };
 
