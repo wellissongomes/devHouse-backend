05243b05ff8940c908df4878e0c2f2aae2237df3
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-20 22:25:04 -0300

    att o metodo store

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,8 +1,21 @@
+import House from '../models/House';
+
 class HouseController {
     async store(req, res) {
-        console.log(req.body);
-        console.log(req.file);
-        return res.json({ ok: true });
+        const { filename } = req.file;
+        const { description, price, location, available } = req.body;
+        const { user_id } = req.headers;
+
+        const house = await House.create({
+            user: user_id,
+            thumbnail: filename,
+            description,
+            price,
+            location,
+            available
+        });
+
+        return res.json(house);
     }
 };
 
