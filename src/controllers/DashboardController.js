a539c5dabe66d65d6457eef426a37c26f89e3d40
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-22 14:03:52 -0300

    remove linha vazia

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -3,7 +3,6 @@ import House from '../models/House';
 const DashboardController = {
     async show(req, res) {
         const { user_id } = req.headers;
-
         const houses = await House.find({ user: user_id });
 
         return res.json(houses);
