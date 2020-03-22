c807b747b9744af94294902119a4ac7ec4b21e99
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-22 14:33:09 -0300

    add nova rota para reservas

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import uploadImg from './config/uploadImg';
 import SessionController from './controllers/SessionController';
 import HouseController from './controllers/HouseController'
 import DashboardController from './controllers/DashboardController';
+import ReserveController from './controllers/ReserveController';
 
 const routes = new Router();
 const upload = multer(uploadImg);
@@ -17,6 +18,8 @@ routes.put('/houses/:house_id', upload.single('thumbnail'), HouseController.upda
 routes.delete('/houses', HouseController.destroy);
 
 routes.get('/dashboard', DashboardController.show);
+
+routes.post('/houses/:house_id/reserve', ReserveController.store);
 
 
 export default routes;
