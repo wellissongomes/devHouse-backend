f8e29bfc09851199f5a6ff6f7bb5990e217a6af1
Wellisson Gomes <wellisson.cacho@ccc.ufcg.edu.br>
2020-03-26 17:12:34 -0300

    add prettier e eslint

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier'
+  ],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    "prettier/prettier": "error",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-underscore-dangle": "off",
+    // "no-unsed-vars": ["error", { "argsIgnorePattern": "next" }]
+  },
+};
