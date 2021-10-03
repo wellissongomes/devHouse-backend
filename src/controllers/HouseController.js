629211d5ca1df5fcafe1a5066162dc131a9c2faf
Ruan Gomes <ruan.alves@ccc.ufcg.edu.br>
2021-10-03 00:24:05 -0300

    house controller refactoring

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,6 +1,26 @@
 import House from '../models/House';
 import User from '../models/User';
 
+const checkAuthorization = (user_id, house_id) => { 
+  const user = await User.findById(user_id);
+  const house = await House.findById(house_id);
+
+  if (String(user._id) !== String(house.user)) return false
+    
+  return true
+}
+
+const getHouseInfosFromRequest = (req) => {
+  return {
+    user: req.headers.user_id,
+    thumbnail: req.file.filename,
+    description: req.body.description,
+    price: req.body.price,
+    location: req.body.location,
+    available: req.body.available,
+  }
+}
+
 const HouseController = {
   async index(req, res) {
     const { available } = req.query;
@@ -10,62 +30,38 @@ const HouseController = {
   },
 
   async store(req, res) {
-    const { filename } = req.file;
-    const { description, price, location, available } = req.body;
-    const { user_id } = req.headers;
+    const houseInfos = getHouseInfosFromRequest(req);
 
-    const house = await House.create({
-      user: user_id,
-      thumbnail: filename,
-      description,
-      price,
-      location,
-      available,
-    });
+    const house = await House.create(houseInfos);
 
     return res.json(house);
   },
-
   async update(req, res) {
-    const { filename } = req.file;
-    const { house_id } = req.params;
-    const { description, price, location, available } = req.body;
-    const { user_id } = req.headers;
+    const { houseId } = req.params;
+    const { userId } = req.headers;
+    const houseInfos = getHouseInfosFromRequest(req);
 
-    const user = await User.findById(user_id);
-    const house = await House.findById(house_id);
-
-    if (String(user._id) !== String(house.user)) {
+    if (!checkAuthorization(userId, houseId)) {
       return res.status(401).json({ error: 'Não autorizado.' });
     }
 
     await House.updateOne(
-      { _id: house_id },
-      {
-        user: user_id,
-        thumbnail: filename,
-        description,
-        price,
-        location,
-        available,
-      }
+      { _id: houseId },
+      houseInfos,
     );
 
     return res.send();
   },
 
   async destroy(req, res) {
-    const { house_id } = req.body;
-    const { user_id } = req.headers;
+    const { houseId } = req.body;
+    const { userId } = req.headers;
 
-    const user = await User.findById(user_id);
-    const house = await House.findById(house_id);
-
-    if (String(user._id) !== String(house.user)) {
+    if (!checkAuthorization(userId, houseId)) {
       return res.status(401).json({ error: 'Não autorizado.' });
     }
 
-    await House.findByIdAndDelete({ _id: house_id });
+    await House.findByIdAndDelete({ _id: houseId });
 
     return res.json({ message: 'Excluida com sucesso' });
   },
